ee5edec6420a2402d388c4a5616ddd448d35a72a
MKISKRA <115628416+MKISKRA@users.noreply.github.com>
2023-02-22 11:30:09 +0000

    Update update_mk100.sh

--- update_mk100.sh
+++ update_mk100.sh
@@ -3,6 +3,6 @@ pkill MK100T; cd /root/mk100/MK100/ ; git fetch --all ; git reset --hard ; git p
 cp /root/mk100/MK100/sys/data_print.cmd /root/mk100/mk100t/data_print.cmd;
 cp /root/mk100/MK100/sys/libinput.rules /lib/udev/rules.d/libinput.rules;
 cp /root/mk100/MK100/sys/gbutton /root/mk100/mk100t/gbutton;
-cp /root/mk100/MK100/akb/XC8_Charger.hex /mk100/firmwares/akb/XC8_Charger.hex;
-cp /root/mk100/MK100/akb/ver.info /mk100/firmwares/akb/ver.info
+cp /root/mk100/MK100/akb/XC8_Charger.hex /root/mk100/firmwares/akb/XC8_Charger.hex;
+cp /root/mk100/MK100/akb/ver.info /root/mk100/firmwares/akb/ver.info
 /root/mk100/mk100t/start.sh
