48ebc17fb6423e96b86828629775809661011676
MKISKRA <115628416+MKISKRA@users.noreply.github.com>
2023-02-22 10:25:02 +0000

    Update update_mk100.sh

--- update_mk100.sh
+++ update_mk100.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-pkill MK100T; cd /root/mk100/MK100/ ; git fetch --all ; git reset --hard ; git pull; cp /root/mk100/MK100/MK100T /root/mk100/mk100t/MK100T; chmod +x /root/mk100/mk100t/MK100T; cp /root/mk100/MK100/sys/data_print.cmd /root/mk100/mk100t/data_print.cmd; /root/mk100/mk100t/start.sh
+pkill MK100T; cd /root/mk100/MK100/ ; git fetch --all ; git reset --hard ; git pull; cp /root/mk100/MK100/MK100T /root/mk100/mk100t/MK100T; chmod +x /root/mk100/mk100t/MK100T;
+cp /root/mk100/MK100/sys/data_print.cmd /root/mk100/mk100t/data_print.cmd;
+cp /root/mk100/MK100/sys/libinput.rules /lib/udev/rules.d/libinput.rules;
+cp /root/mk100/MK100/sys/gbutton /root/mk100/mk100t/gbutton;
+/root/mk100/mk100t/start.sh
