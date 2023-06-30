c50da9cd4549fc9f5aadd95b822a6e9a3605c1c9
MKISKRA <115628416+MKISKRA@users.noreply.github.com>
2023-06-30 08:03:43 +0000

    Update update_mk100.sh

--- update_mk100.sh
+++ update_mk100.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 pkill MK100T; cd /root/mk100/MK100/ ; git fetch --all ; git reset --hard ; git pull; cp /root/mk100/MK100/MK100T /root/mk100/mk100t/MK100T; chmod +x /root/mk100/mk100t/MK100T;
 cp /root/mk100/MK100/sys/data_print.cmd /root/mk100/mk100t/data_print.cmd;
+cp /root/mk100/MK100/sys/bl_scan.sh /root/mk100/mk100t/bl_scan.sh;
+cp /root/mk100/MK100/sys/ping.sh /root/mk100/mk100t/ping.sh;
+cp /root/mk100/MK100/sys/speedtest.sh /root/mk100/mk100t/speedtest.sh;
 cp /root/mk100/MK100/sys/libinput.rules /lib/udev/rules.d/libinput.rules;
 cp /root/mk100/MK100/sys/gbutton /root/mk100/mk100t/gbutton;
 cp /root/mk100/MK100/sys/sleep_screen.sh /root/mk100/mk100t/sleep_screen.sh;
